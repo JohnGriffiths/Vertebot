ef95cc30a25383b726ab835d75411b21c2765849
John Griffiths <j.davidgriffiths@gmail.com>
2015-02-23 10:16:03 +1100

    removing; replaced with .md file

--- README.m
+++
@@ -1,8 +0,0 @@
-
-# Vertebot
-
-A brain-based device or 'neurorobot' is a machine with a control system that resembles a biological brain. This repo and the [website](http://www.vertebot.com/) provide instructions, discussion and code for building a neurorobot. The long-term aim is to develop a robot with an artificial vertebrate brain: a 'vertebot'. 
-
-
-
-
